b5b08e114e60db5b7477c907e226d74c45aca1fa
tnt2ray <tnt2ray@9f0fa5d4-349d-49a9-8f98-7d1f2c84a895>
2008-12-04 12:14:03 +0000

    Broker_SysMon: add zhcn and zhtw

---
+++ Locales/enUS.lua
@@ -0,0 +1,20 @@
+
+local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Broker_SysMon", "enUS", true)
+if not L then return end
+
+-- Broker_FPS
+L["Broker_FPS"] = true
+L["fps"] = true
+L["Framerate"] = true
+-- Broker_Latency
+L["Broker_Latency"] = true
+L["ms"] = true
+L["Latency"] = true
+-- Broker_MemUse
+L["Broker_MemUse"] = true
+L["MiB"] = true
+L["Memory Usage"] = true
+-- Broker_IncreasingRate
+L["Broker_IncreasingRate"] = true
+L["KiB/s"] = true
+L["Increasing Rate"] = true
