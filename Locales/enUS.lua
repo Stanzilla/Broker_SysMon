b013c1705247ca5164b6e74ba83ebc7b4f697ac2
folk <233791+folknor@users.noreply.github.com>
2010-10-14 23:12:04 +0000

    Toc bump and some renaming, apparently.

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -3,20 +3,16 @@ local L = LibStub:GetLibrary("AceLocale-3.0"):NewLocale("Broker_SysMon", "enUS",
 if not L then return end
 
 -- Broker_FPS
-L["Broker_FPS"] = true
 L["fps"] = true
 L["Framerate"] = true
 -- Broker_Latency
-L["Broker_Latency"] = true
 L["ms"] = true
 L["Latency"] = true
 -- Broker_MemUse
-L["Broker_MemUse"] = true
 L["mb"] = true
-L["Memory Usage"] = true
+L["Memory usage"] = true
 L["List above shows the top %d addons with regards to memory usage."] = true
 -- Broker_IncreasingRate
-L["Broker_IncreasingRate"] = true
 L["kbs"] = true
-L["Increasing Rate"] = true
+L["Increasing rate"] = true
 
