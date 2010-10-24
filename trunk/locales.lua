329382acd4d26286d128e8cf16e53312b9871dbe
rabbit <rabbit@9f0fa5d4-349d-49a9-8f98-7d1f2c84a895>
2010-10-24 09:14:01 +0000

    Use the localization system since it's such a tiny addon.

---
+++ trunk/locales.lua
@@ -0,0 +1,52 @@
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+local AL = LibStub("AceLocale-3.0")
+local L = nil
+
+L = AL:NewLocale("Broker_SysMon", "enUS", true, debug)
+if L then
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "deDE")
+if L then
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "esES")
+if L then
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "esMX")
+if L then
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "frFR")
+if L then
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "koKR")
+if L then
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "ruRU")
+if L then
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "zhCN")
+if L then
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
+L = AL:NewLocale("Broker_SysMon", "zhTW")
+if L then
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="concat")@
+end
+
